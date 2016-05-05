2b175d05c90324d593b778a1732793cd61ae74f6
g5qinyix <eggs@live.ca>
2016-05-05 17:17:16 -0400

    Added select2 stuff to make search bar work. Still has issues though.

---
+++ lolserver/lolserver/api/masteryPointFormula.py
@@ -0,0 +1,25 @@
+import math
+
+def pointsRequired(currentPoints, desiredPoints, winRate):
+    """ 
+    Assume each game is 35 minutes long. 
+    Given the user's current champion mastery points (currentPoints),
+    their desired mastery points for a champion (desiredPoints),
+    and a winrate, determine the number of games required (on average) to reach
+    the desired champion mastery point goal.
+    """
+    
+    # pointsForWin and pointsForLoss assumes 35 minute games. Numbers are 
+    # determined using a regression model (see the Excel file).
+    pointsForWin = 1256.58792651
+    pointsForLoss = 222.266550523
+
+
+    neededPoints = desiredPoints - currentPoints
+    if neededPoints <= 0:
+        return 0
+    else:
+        avgPointPerGame = winRate * pointsForWin + (1 - winRate) * pointsForLoss
+        return int(math.ceil(neededPoints / avgPointPerGame))
+    
+    
