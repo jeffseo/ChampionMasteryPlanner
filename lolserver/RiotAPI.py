1641e08518bf16be538261f89b4b0a47f7e51528
g5qinyix <eggs@live.ca>
2016-05-09 04:06:39 -0400

    Moved functions to proper place.

--- lolserver/RiotAPI.py
+++ lolserver/RiotAPI.py
@@ -208,35 +208,6 @@ class RiotAPI(object):
                                                         version=Consts.API_VERSIONS['game'],
                                                         summonerId=summonerId)
         matchesJson = self._request(api_url)['games']
-
-        gamesByDate ={}
-        for game in matchesJson:
-            if game['subType'] in Consts.VALID_GAME_MODES:
-                dateOfGame = game['createDate']
-                dateStamp = time.ctime(int(dateOfGame) / 1000)
-                parsedDateStamp = dateStamp[4:10] + ", " + dateStamp[20:]                
-                if parsedDateStamp in gamesByDate:
-                    # Formula used to get estimate of Champion Mastery Point gain.
-                    gamesByDate[parsedDateStamp] += masteryPointFormula.pointsForGame((float(game['stats']['timePlayed']) / 60), bool(game['stats']['win']))
-                else:
-                    gamesByDate[parsedDateStamp] = masteryPointFormula.pointsForGame((float(game['stats']['timePlayed']) / 60), bool(game['stats']['win']))
-        prepDataForJS = []
-        dateLabels = []
-        pointsGained = []
-        for day in gamesByDate:
-            dateLabels.append(day)
-        
-        # dicts do not preserve order. The code below accounts for this.
-        dateLabels.sort()
-        for date in dateLabels:
-            pointsGained.append(gamesByDate[date])
-            
-        prepDataForJS.append(dateLabels)
-        prepDataForJS.append(pointsGained)
-        print(prepDataForJS[0])
-        for date in prepDataForJS[0]:
-            print(type(date))
-        return prepDataForJS
-
+        return matchesJson
 
         
--- lolserver/lolserver/api/views.py
+++ lolserver/lolserver/api/views.py
@@ -6,10 +6,37 @@ from django.template import RequestContext
 import RiotConstants
 from RiotAPI import RiotAPI
 import masteryPointFormula
+import time
 # Create your views here.
 
 def index(request):
     return render(request,'templates/main.html')
+
+def parseRecentMatches(matchesJson):
+        gamesByDate = {}
+        for game in matchesJson:
+            if game['subType'] in RiotConstants.VALID_GAME_MODES:
+                dateOfGame = game['createDate']
+                dateStamp = time.ctime(int(dateOfGame) / 1000)
+                parsedDateStamp = dateStamp[4:10] + ", " + dateStamp[20:]                
+                if parsedDateStamp in gamesByDate:
+                    # Formula used to get estimate of Champion Mastery Point gain.
+                    gamesByDate[parsedDateStamp] += masteryPointFormula.pointsForGame((float(game['stats']['timePlayed']) / 60), bool(game['stats']['win']))
+                else:
+                    gamesByDate[parsedDateStamp] = masteryPointFormula.pointsForGame((float(game['stats']['timePlayed']) / 60), bool(game['stats']['win']))
+        prepDataForJS = []
+        dateLabels = []
+        pointsGained = []
+        for day in gamesByDate:
+            dateLabels.append(day) 
+            
+        # dicts do not preserve order. The code below accounts for this.
+        dateLabels.sort()
+        for date in dateLabels:
+            pointsGained.append(gamesByDate[date])
+        prepDataForJS.append(dateLabels)
+        prepDataForJS.append(pointsGained)
+        return prepDataForJS        
 
 def summoner(request):
     context = {}
@@ -42,8 +69,9 @@ def summoner(request):
             # list of the labels for the graphs (days) and the second is a list
             # of champion point values corresponding to the day labels.
             recentMatchesData = api.getRecentMatches(summonerId)
-            context['graphLabels'] = recentMatchesData[0]
-            context['graphData'] = recentMatchesData[1]
+            recentMatchesDataParsed = parseRecentMatches(recentMatchesData)
+            context['graphLabels'] = recentMatchesDataParsed[0]
+            context['graphData'] = recentMatchesDataParsed[1]
             return render(request,'templates/summoner.html', context)          
         except TypeError:
             print("TypeError")
